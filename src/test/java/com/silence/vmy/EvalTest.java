9c8f016a0e983216cc60ddf7fb8afef09eedd863
forsilence <3312034934@qq.com>
2022-10-06 14:10:16 +0800

    change test module

--- src/test/java/com/silence/vmy/EvalTest.java
+++ src/test/java/com/silence/vmy/EvalTest.java
@@ -11,15 +11,15 @@ public class EvalTest {
 
   @Test
   public void evalTest(){
-    assertEquals( 1 + 2 * (3 + 4), Eval.eval("1 + 2 * ( 3 + 4 )"));
-    assertEquals( 1 + 2 * (3 + 4.0), Eval.eval("1 + 2 * ( 3 + 4.0 )"));
-    assertEquals( 1 + 2 * (3 + 4) * (5) - 1, Eval.eval("1 + 2 * ( 3 + 4 ) * ( 5 )  - 1"));
+    assertEquals( 1 + 2 * (3 + 4), Eval.eval("1 + 2 * ( 3 + 4 )", AST.variableStoreTreeEvaluator()));
+    assertEquals( 1 + 2 * (3 + 4.0), Eval.eval("1 + 2 * ( 3 + 4.0 )", AST.variableStoreTreeEvaluator()));
+    assertEquals( 1 + 2 * (3 + 4) * (5) - 1, Eval.eval("1 + 2 * ( 3 + 4 ) * ( 5 )  - 1", AST.variableStoreTreeEvaluator()));
   }
   @Test
   public void evalTest1(){
-    assertEquals(1 + 2 * 3 * 4, Eval.eval("1 + 2 * 3 * 4"));
-    assertEquals(1 + 2 / 3 * 4 * 7, Eval.eval("1 + 2 / 3 * 4 * 7"));
-    assertEquals(1 + 14 / (3 + 4), Eval.eval("1 + 14 / (3 + 4)"));
+    assertEquals(1 + 2 * 3 * 4, Eval.eval("1 + 2 * 3 * 4", AST.variableStoreTreeEvaluator()));
+    assertEquals(1 + 2 / 3 * 4 * 7, Eval.eval("1 + 2 / 3 * 4 * 7", AST.variableStoreTreeEvaluator()));
+    assertEquals(1 + 14 / (3 + 4), Eval.eval("1 + 14 / (3 + 4)", AST.variableStoreTreeEvaluator()));
   }
 
   @Test
