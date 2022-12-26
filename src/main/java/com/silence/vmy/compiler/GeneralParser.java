fcb1847a9dcc798313b97672b9fd3b6879ce5d8d
forsilence <3312034934@qq.com>
2022-12-26 22:43:14 +0800

    unfinished new Parser

--- src/main/java/com/silence/vmy/compiler/GeneralParser.java
+++ src/main/java/com/silence/vmy/compiler/GeneralParser.java
@@ -1,13 +1,58 @@
 package com.silence.vmy.compiler;
 
-import com.silence.vmy.compiler.tree.Root;
+import com.silence.vmy.compiler.tree.*;
+
+import java.util.List;
+import java.util.Stack;
 
 public class GeneralParser implements Parser{
   private Lexer lexer;
+  private Stack<BaseTree> nodes;
   @Override
   public Root parse() {
     return null;
   }
 
-//  protected
+  /**
+   * e_fun = fun identifier expr "{" e_block "}"
+   */
+  private FunctionDecl compileFunc(){
+    return null;
+  }
+
+  /**
+   * expr = "(" ")" | "(" expr2 ")"
+   */
+  private Expression expr(){
+    return null;
+  }
+
+  /**
+   * expr2 =  expr3 | expr2 "," expr3
+   */
+  private List<Exception> expr2(){
+    return null;
+  }
+
+  /**
+   * e_block = [ expression ]
+   */
+  private BlockStatement compileBlock(){
+    return null;
+  }
+
+  /**
+   * expr3 = varDecl "=" expr4
+   */
+  private Expression expr3(){
+    return null;
+  }
+
+  /**
+   * call = identifier expr
+   */
+  private Expression call(){
+    return null;
+  }
+
 }
