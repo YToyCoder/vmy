a8e5cbe2d3b5e56d8ff3d11ac6c2bf4d3ce0a5ff
forsilence <3312034934@qq.com>
2022-12-26 22:40:18 +0800

    new visitor

--- src/main/java/com/silence/vmy/compiler/tree/TreeVisitor.java
+++ src/main/java/com/silence/vmy/compiler/tree/TreeVisitor.java
@@ -1,4 +1,12 @@
 package com.silence.vmy.compiler.tree;
 
-public interface TreeVisitor<T> {
+public interface TreeVisitor<R,T> {
+  R visitLiteral(LiteralExpression expression, T payload);
+  R visitUnary(Unary expression, T payload);
+  R visitBlock(BlockStatement statement, T payload);
+  R visitBinary(BinaryOperateExpression expression, T payload);
+  R visitVariableDecl(VariableDecl expression, T payload);
+  R visitAssignment(AssignmentExpression expression, T payload);
+  R visitFunctionDecl(FunctionDecl function, T payload);
+  R visitRoot(Root root, T payload);
 }
