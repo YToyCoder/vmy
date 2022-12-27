5b8c4d517e91b6386a340c40171d94913cb81bac
forsilence <3312034934@qq.com>
2022-12-27 14:05:50 +0800

    Parser Things

--- src/main/java/com/silence/vmy/compiler/GeneralParser.java
+++ src/main/java/com/silence/vmy/compiler/GeneralParser.java
@@ -2,12 +2,16 @@ package com.silence.vmy.compiler;
 
 import com.silence.vmy.compiler.tree.*;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 
 public class GeneralParser implements Parser{
   private Lexer lexer;
   private Stack<BaseTree> nodes;
+  private Tokens.Token token;
+  private Tokens.Token pre;
+  private List<Tokens.Token> savedTokens = new LinkedList<>();
   @Override
   public Root parse() {
     return null;
@@ -27,6 +31,29 @@ public class GeneralParser implements Parser{
     return null;
   }
 
+  protected Tokens.Token next(){
+    pre = token;
+    return (token = savedTokens.isEmpty() ? lexer.next() : savedTokens.remove(0));
+  }
+
+  protected Tokens.Token token(){
+    return token(0);
+  }
+
+  protected Tokens.Token token(int lookahead) {
+    if(lookahead == 0) {
+      return token;
+    }else {
+      ensureLookahead(lookahead);
+      return savedTokens.get(lookahead - 1);
+    }
+  }
+
+  protected void ensureLookahead(int lookahead) {
+    for(int i= savedTokens.size(); i < lookahead && lexer.hasNext() ; i++)
+      savedTokens.add(lexer.next());
+  }
+
   /**
    * expr2 =  expr3 | expr2 "," expr3
    */
@@ -42,11 +69,50 @@ public class GeneralParser implements Parser{
   }
 
   /**
-   * expr3 = varDecl "=" expr4
+   * expr3 = identifier "=" expr4
    */
   private Expression expr3(){
     return null;
   }
+
+  /**
+   * expression = varDecl "=" expr4
+   *            | expr3
+   */
+  private Expression expression(){
+    return null;
+  }
+
+  /**
+   * one = identifier | literal | "(" multi ")" | call
+   */
+  Expression one(){
+    return null;
+  }
+
+  /**
+   * literal = "true" | "false"
+   *         | numberLiteral
+   *         | stringLiteral
+   *         | charLiteral
+   *         | functionLiteral
+   */
+  Expression literal(){
+    return null;
+  }
+
+  /**
+   * add = multi
+   *    | multi "+" multi
+   *    | multi "-" multi
+   */
+  Expression add(){
+    return null;
+  }
+
+  /**
+   * multi = one | multi "*" one | multi "/" one
+   */
 
   /**
    * call = identifier expr
