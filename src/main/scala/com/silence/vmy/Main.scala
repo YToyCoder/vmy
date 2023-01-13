f9c6866827601960d6ae5e14c6d183543357f270
forsilence <3312034934@qq.com>
2023-01-13 20:37:18 +0800

    scala3

--- src/main/scala/com/silence/vmy/Main.scala
+++ src/main/scala/com/silence/vmy/Main.scala
@@ -2,6 +2,11 @@ package com.silence.vmy
 
 object ScalaMain {
   def f(): Unit = {
-    println("hello")
+
+    for i <- List("AN","BM")
+        if i(0) == 'A'
+       el <- i
+    do println(s"scala ${i} - ${el}")
+
   }
 }
