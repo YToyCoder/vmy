520eb718a71afc0eec62827a5207222ebb672510
forsilence <3312034934@qq.com>
2023-04-18 11:16:16 +0800

    add comparing op mapping to string

--- src/main/java/com/silence/vmy/compiler/tree/Tree.java
+++ src/main/java/com/silence/vmy/compiler/tree/Tree.java
@@ -4,84 +4,86 @@ import com.silence.vmy.compiler.visitor.NodeVisitor;
 
 public interface Tree {
 
-    // old version
-    default void accept(NodeVisitor visitor){
-        accept(visitor);
-    }
+	// old version
+	default void accept(NodeVisitor visitor){ accept(visitor); }
+	// new version
+	<R,T> R accept(TreeVisitor<R,T> visitor, T payload);
+	<T> Tree accept(TVisitor<T> visitor, T t);
+	long position();
+	Tag tag();
 
-    // new version
-    <R,T> R accept(TreeVisitor<R,T> visitor, T payload);
-    <T> Tree accept(TVisitor<T> visitor, T t);
+	enum Tag{
+		Root,
+		// ops
+		Add,
+		AddEqual, // +=
+		Concat, // ++
+		Sub, // -
+		SubEqual, // -=
+		Multi, // *
+		MultiEqual, // *=
+		Div, // /
+		DivEqual, // /=
+		Assign,
 
-    long position();
+		/* comparations */
+		Equal, // ==
+		NotEqual, // !=
+		Less, // <
+		Greater, // >
+		Le, // <=
+		Ge, // >=
 
-    Tag tag();
+		// literal
+		IntLiteral,
+		StringLiteral,
+		DoubleLiteral,
+		FunctionLiteral,
+		//
+		Param,
+		Id,
+		TypeDecl,
+		CallExpr,
 
-    enum Tag{
-        Root,
-        // ops
-        Add,
-        AddEqual, // +=
-        Concat, // ++
-        Sub, // -
-        SubEqual, // -=
-        Multi, // *
-        MultiEqual, // *=
-        Div, // /
-        DivEqual, // /=
-        Assign,
+		// reserved keys
+		/** declaration */
+		Fun,
+		VarDecl,
+		/** condition */
+		If,
+		Elif,
+		Else,
+		/** loop */
+		For
+	};
 
-        /* comparations */
-        Equal, // ==
-        NotEqual, // !=
-        Less, // <
-        Greater, // >
-        Le, // <=
-        Ge, // >=
-
-        // literal
-        IntLiteral,
-        StringLiteral,
-        DoubleLiteral,
-        FunctionLiteral,
-        //
-        Param,
-        Id,
-        TypeDecl,
-        CallExpr,
-
-        // reserved keys
-        /** declaration */
-        Fun,
-        VarDecl,
-        /** condition */
-        If,
-        Elif,
-        Else,
-        /** loop */
-        For
-    };
-
-    static String opTag2String(Tag tag){
-        return switch (tag){
-            /* operator */
-            case Add -> "+";
-            case AddEqual -> "+=";
-            case Sub -> "-";
-            case Div -> "/";
-            case SubEqual -> "-=";
-            case Multi -> "*";
-            case Assign -> "=";
-            case MultiEqual -> "*=";
-            case DivEqual -> "/=";
-            case Concat -> "++";
-            /* condition */
-            case If -> "if";
-            case Elif -> "elif";
-            case Else -> "else";
-            /* loop */
-            case For -> "for";
-            default -> ">> no mapping tag <<";
-        };
-    }
+	static String opTag2String(Tag tag){
+		return switch (tag){
+			/* operator */
+			case Add -> "+";
+			case AddEqual -> "+=";
+			case Sub -> "-";
+			case Div -> "/";
+			case SubEqual -> "-=";
+			case Multi -> "*";
+			case Assign -> "=";
+			case MultiEqual -> "*=";
+			case DivEqual -> "/=";
+			case Concat -> "++";
+			/* condition */
+			case If -> "if";
+			case Elif -> "elif";
+			case Else -> "else";
+			/* loop */
+			case For -> "for";
+			/* comparations */
+			case Equal -> "=="; // ==
+			case NotEqual -> "!="; // !=
+			case Less -> "<"; // <
+			case Greater -> ">"; // >
+			case Le -> "<="; // <=
+			case Ge -> ">="; // >=
+			default -> ">> no mapping tag <<";
+		};
+	}
 }
