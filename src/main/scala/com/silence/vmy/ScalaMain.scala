2e3469f7bb8025cb10a68afb4f2555f6ecfeb319
forsilence <3312034934@qq.com>
2023-04-21 23:30:00 +0800

    better log

--- src/main/scala/com/silence/vmy/ScalaMain.scala
+++ src/main/scala/com/silence/vmy/ScalaMain.scala
@@ -2,22 +2,28 @@ package com.silence.vmy
 
 import com.silence.vmy.evaluate.TreeEmulator
 import com.silence.vmy.tools.Eval
+import com.silence.vmy.tools.Log
+import com.silence.vmy.compiler.ConstFold
 
-object ScalaMain {
+object ScalaMain extends Log {
   val debug = true
 
   private def evalScript(script: String) : Unit = {
-    if( debug )
+    if( debug ) {
       println(s"parsing script ${script}")
+    }
     val ast = Eval.parsing(script, true)
+    val foldTree = ast.accept(new ConstFold(), 0)
     if(debug) {
-      println(ast)
-      println("parsing finished")
-      println("starting eval ...")
+      log(ast.toString)
+      log("#" * 20)
+      log(foldTree.toString)
+      log("parsing finished")
+      log("starting eval ...")
     }
     ast.accept(new TreeEmulator(), null)
     if(debug)
-      println("eval finished")
+      log("eval finished")
   }
 
   def main(args: Array[String]): Unit = {
