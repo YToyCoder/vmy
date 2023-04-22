9b1b2b554dcd326e249168777ea3d92d8ab44ac5
forsilence <3312034934@qq.com>
2023-04-22 15:03:49 +0800

    not used current

--- src/main/scala/com/silence/vmy/compiler/TypeSystems.scala
+++ src/main/scala/com/silence/vmy/compiler/TypeSystems.scala
@@ -37,62 +37,62 @@ object BuiltinTypeString {
   val BooleanT = "boolean"
   val LongT = "long"
 }
-
-class TypeCheck extends TreeVisitor[CompilingPhaseType, CompilingPhaseType]{
-
-  override def visitLiteral(expression: LiteralExpression, payload: CompilingPhaseType): CompilingPhaseType = {
-    expression.tag() match {
-      case Tree.Tag.DoubleLiteral =>   DoubleT
-      case Tree.Tag.StringLiteral =>   StringT
-      case Tree.Tag.IntLiteral =>      IntT
-      case Tree.Tag.FunctionLiteral => null // todo
-      case _ => null
-    }
-  }
-
-  override def visitUnary(expression: Unary, payload: CompilingPhaseType): CompilingPhaseType =
-    expression.body().accept(this, null)
-
-  override def visitBlock(statement: BlockStatement, payload: CompilingPhaseType): CompilingPhaseType = null
-
-  override def visitBinary(expression: BinaryOperateExpression, payload: CompilingPhaseType): CompilingPhaseType = {
-    // visit both and compare
-    val lTp = expression.left().accept(this, payload)
-    val rTp = expression.right().accept(this, payload)
-    null
-  }
-
-  override def visitForStatement(statement: ForStatement, payload: CompilingPhaseType): CompilingPhaseType= {
-    null
-  }
-
-  private def expressionAsType(expr: TypeExpr) : CompilingPhaseType = {
-    expr.typeId() match {
-      case BuiltinTypeString.StrT => StringT
-      case BuiltinTypeString.DoubleT => DoubleT
-      case BuiltinTypeString.IntT => IntT
-      case id => NamedT(id)
-    }
-  }
-
-  override def visitVariableDecl(expression: VariableDecl, payload: CompilingPhaseType): CompilingPhaseType = expressionAsType(expression.t())
-
-  override def visitAssignment(expression: AssignmentExpression, payload: CompilingPhaseType): CompilingPhaseType = {
-    val variable = expression.left().accept(this, payload)
-    val expr     = expression.right().accept(this, payload)
-    null
-  }
-  override def visitFunctionDecl(function: FunctionDecl, payload: CompilingPhaseType): CompilingPhaseType = null
-  override def visitRoot(root: Root, payload: CompilingPhaseType): CompilingPhaseType = null
-  override def visitListExpr[E <: Expression](expr: ListExpr[E], payload: CompilingPhaseType): CompilingPhaseType = null
-  override def visitReturnExpr(expr: ReturnExpr, payload: CompilingPhaseType): CompilingPhaseType = null
-  override def visitTypeExpr(expr: TypeExpr, payload: CompilingPhaseType): CompilingPhaseType = null
-  override def visitCallExpr(expr: CallExpr, payload: CompilingPhaseType): CompilingPhaseType = null
-  override def visitIdExpr(expr: IdExpr, payload: CompilingPhaseType): CompilingPhaseType = null
-  override def visitIfStatement(statement: IfStatement, payload: CompilingPhaseType): CompilingPhaseType = null
-  override def visitArr(expr: ArrExpression, payload: CompilingPhaseType): CompilingPhaseType = null
-}
-
-object TypeCheck {
-  def apply(): TypeCheck = new TypeCheck()
-}
+//
+// class TypeCheck extends TreeVisitor[CompilingPhaseType, CompilingPhaseType]{
+//
+//   override def visitLiteral(expression: LiteralExpression, payload: CompilingPhaseType): CompilingPhaseType = {
+//     expression.tag() match {
+//       case Tree.Tag.DoubleLiteral =>   DoubleT
+//       case Tree.Tag.StringLiteral =>   StringT
+//       case Tree.Tag.IntLiteral =>      IntT
+//       case Tree.Tag.FunctionLiteral => null // todo
+//       case _ => null
+//     }
+//   }
+//
+//   override def visitUnary(expression: Unary, payload: CompilingPhaseType): CompilingPhaseType =
+//     expression.body().accept(this, null)
+//
+//   override def visitBlock(statement: BlockStatement, payload: CompilingPhaseType): CompilingPhaseType = null
+//
+//   override def visitBinary(expression: BinaryOperateExpression, payload: CompilingPhaseType): CompilingPhaseType = {
+//     // visit both and compare
+//     val lTp = expression.left().accept(this, payload)
+//     val rTp = expression.right().accept(this, payload)
+//     null
+//   }
+//
+//   override def visitForStatement(statement: ForStatement, payload: CompilingPhaseType): CompilingPhaseType= {
+//     null
+//   }
+//
+//   private def expressionAsType(expr: TypeExpr) : CompilingPhaseType = {
+//     expr.typeId() match {
+//       case BuiltinTypeString.StrT => StringT
+//       case BuiltinTypeString.DoubleT => DoubleT
+//       case BuiltinTypeString.IntT => IntT
+//       case id => NamedT(id)
+//     }
+//   }
+//
+//   override def visitVariableDecl(expression: VariableDecl, payload: CompilingPhaseType): CompilingPhaseType = expressionAsType(expression.t())
+//
+//   override def visitAssignment(expression: AssignmentExpression, payload: CompilingPhaseType): CompilingPhaseType = {
+//     val variable = expression.left().accept(this, payload)
+//     val expr     = expression.right().accept(this, payload)
+//     null
+//   }
+//   override def visitFunctionDecl(function: FunctionDecl, payload: CompilingPhaseType): CompilingPhaseType = null
+//   override def visitRoot(root: Root, payload: CompilingPhaseType): CompilingPhaseType = null
+//   override def visitListExpr[E <: Expression](expr: ListExpr[E], payload: CompilingPhaseType): CompilingPhaseType = null
+//   override def visitReturnExpr(expr: ReturnExpr, payload: CompilingPhaseType): CompilingPhaseType = null
+//   override def visitTypeExpr(expr: TypeExpr, payload: CompilingPhaseType): CompilingPhaseType = null
+//   override def visitCallExpr(expr: CallExpr, payload: CompilingPhaseType): CompilingPhaseType = null
+//   override def visitIdExpr(expr: IdExpr, payload: CompilingPhaseType): CompilingPhaseType = null
+//   override def visitIfStatement(statement: IfStatement, payload: CompilingPhaseType): CompilingPhaseType = null
+//   override def visitArr(expr: ArrExpression, payload: CompilingPhaseType): CompilingPhaseType = null
+// }
+//
+// object TypeCheck {
+//   def apply(): TypeCheck = new TypeCheck()
+// }
