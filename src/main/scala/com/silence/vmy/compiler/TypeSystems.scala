f9b946cf186d24ceaf398da33a14853ba855afb0
forsilence <3312034934@qq.com>
2023-04-24 18:34:07 +0800

    type system

--- src/main/scala/com/silence/vmy/compiler/TypeSystems.scala
+++ src/main/scala/com/silence/vmy/compiler/TypeSystems.scala
@@ -1,26 +1,9 @@
 package com.silence.vmy.compiler
 
 import com.silence.vmy.compiler.tree._
-// {
-  // AssignmentExpression, 
-  // BinaryOperateExpression, 
-  // BlockStatement, 
-  // CallExpr, 
-  // Expression, 
-  // FunctionDecl, 
-  // IdExpr, 
-  // IfStatement, 
-  // ListExpr, 
-  // LiteralExpression, 
-  // ReturnExpr, 
-  // Root, 
-  // Tree, 
-  // TreeVisitor, 
-  // TypeExpr, 
-  // Unary, 
-  // VariableDecl,
-  // ArrExpression
-// }
+
+import scala.collection.mutable.Stack
+import scala.collection.mutable
 
 sealed class TheType(val name:String) extends CompilingPhaseType
 
@@ -28,71 +11,93 @@ object StringT extends TheType("string")
 object DoubleT extends TheType("double")
 object IntT extends TheType("int")
 case class FunT(val paramTs: Array[TheType], val ret: TheType) extends TheType("fun")
-case class NamedT(override val name: String) extends TheType(name)
+case class ArrT(val typeVar: TheType)
+case class ReType(realType: TheType) extends TheType(s"ret => ${realType.name}")
+object NullExistType extends TheType("TypeNotExist")
+object VoidT extends TheType("void")
 
 object BuiltinTypeString {
-  val StrT = "string"
-  val DoubleT = "double"
-  val IntT = "int"
-  val BooleanT = "boolean"
-  val LongT = "long"
+  val StrT = "String"
+  val DoubleT = "Double"
+  val IntT = "Int"
+  val BooleanT = "Boolean"
+  val LongT = "Long"
 }
-//
-// class TypeCheck extends TreeVisitor[CompilingPhaseType, CompilingPhaseType]{
-//
-//   override def visitLiteral(expression: LiteralExpression, payload: CompilingPhaseType): CompilingPhaseType = {
-//     expression.tag() match {
-//       case Tree.Tag.DoubleLiteral =>   DoubleT
-//       case Tree.Tag.StringLiteral =>   StringT
-//       case Tree.Tag.IntLiteral =>      IntT
-//       case Tree.Tag.FunctionLiteral => null // todo
-//       case _ => null
-//     }
-//   }
-//
-//   override def visitUnary(expression: Unary, payload: CompilingPhaseType): CompilingPhaseType =
-//     expression.body().accept(this, null)
-//
-//   override def visitBlock(statement: BlockStatement, payload: CompilingPhaseType): CompilingPhaseType = null
-//
-//   override def visitBinary(expression: BinaryOperateExpression, payload: CompilingPhaseType): CompilingPhaseType = {
-//     // visit both and compare
-//     val lTp = expression.left().accept(this, payload)
-//     val rTp = expression.right().accept(this, payload)
-//     null
-//   }
-//
-//   override def visitForStatement(statement: ForStatement, payload: CompilingPhaseType): CompilingPhaseType= {
-//     null
-//   }
-//
-//   private def expressionAsType(expr: TypeExpr) : CompilingPhaseType = {
-//     expr.typeId() match {
-//       case BuiltinTypeString.StrT => StringT
-//       case BuiltinTypeString.DoubleT => DoubleT
-//       case BuiltinTypeString.IntT => IntT
-//       case id => NamedT(id)
-//     }
-//   }
-//
-//   override def visitVariableDecl(expression: VariableDecl, payload: CompilingPhaseType): CompilingPhaseType = expressionAsType(expression.t())
-//
-//   override def visitAssignment(expression: AssignmentExpression, payload: CompilingPhaseType): CompilingPhaseType = {
-//     val variable = expression.left().accept(this, payload)
-//     val expr     = expression.right().accept(this, payload)
-//     null
-//   }
-//   override def visitFunctionDecl(function: FunctionDecl, payload: CompilingPhaseType): CompilingPhaseType = null
-//   override def visitRoot(root: Root, payload: CompilingPhaseType): CompilingPhaseType = null
-//   override def visitListExpr[E <: Expression](expr: ListExpr[E], payload: CompilingPhaseType): CompilingPhaseType = null
-//   override def visitReturnExpr(expr: ReturnExpr, payload: CompilingPhaseType): CompilingPhaseType = null
-//   override def visitTypeExpr(expr: TypeExpr, payload: CompilingPhaseType): CompilingPhaseType = null
-//   override def visitCallExpr(expr: CallExpr, payload: CompilingPhaseType): CompilingPhaseType = null
-//   override def visitIdExpr(expr: IdExpr, payload: CompilingPhaseType): CompilingPhaseType = null
-//   override def visitIfStatement(statement: IfStatement, payload: CompilingPhaseType): CompilingPhaseType = null
-//   override def visitArr(expr: ArrExpression, payload: CompilingPhaseType): CompilingPhaseType = null
-// }
-//
-// object TypeCheck {
-//   def apply(): TypeCheck = new TypeCheck()
-// }
+
+case class TypeContext(pre: TypeContext) {
+  private val stack : Stack[TheType] = Stack()
+  // when lookup failed , it should return NullExistType
+  private val map : mutable.Map[String, TheType] = mutable.Map().withDefault(_ => NullExistType)
+  def push(t: TheType): TypeContext = {
+    stack.push(t) 
+    this
+  } 
+  def pop(): TheType = stack.pop()
+  def put(name: String, t: TheType): TypeContext = {
+    map(name) = t
+    this
+  }
+  def lookup(name: String): TheType = map(name)
+}
+
+
+class TypeChecker extends TVisitor[TheType] {
+  private val TypeOrderMap: Map[TheType, Int] = 
+    Map((IntT, 0), (DoubleT, 1), (StringT, 2)).withDefault(_ => -1)
+  private var typeContextFrame : TypeContext = null
+  private def createFrame(): TypeChecker = {
+    typeContextFrame = TypeContext(typeContextFrame) 
+    this
+  }
+  private def exitFrame(): TypeChecker = {
+    if(
+      typeContextFrame != null && 
+      typeContextFrame.pre != null
+    ) {
+      typeContextFrame = typeContextFrame.pre
+    }
+    this
+  }
+
+  private def popType(): TheType = typeContextFrame.pop()
+  private def pushType(t: TheType) = {
+    typeContextFrame.push(t)
+    this
+  }
+
+  override def leaveIdExpr(exp: IdExpr, i: TheType) = {
+    pushType{ typeContextFrame.lookup(exp.name()) }
+    exp
+  }
+
+  override def leaveUnary(exp: Unary, i: TheType) = {
+    val body = exp.body.accept(this, i)
+    val bodyType = popType()
+    pushType(bodyType)
+    exp
+  }
+
+  private def biggerType(a: TheType, b: TheType): TheType = {
+    (TypeOrderMap(a), TypeOrderMap(b)) match{
+      case (-1, _) | (_, -1)=> NullExistType
+      case (aOrder, bOrder) => if(aOrder > bOrder) a else b
+    }
+  }
+
+  override def leaveBlock(state: BlockStatement, t: TheType) = {
+    val states = state.exprs
+    var retType = VoidT
+    for(i <- 0 until states.size()){
+      states.get(i).accept(this, t)
+      val stateType = popType()
+      if(stateType.isInstanceOf[ReType]) {
+        retType = biggerType(stateType, retType) match {
+          case NullExistType => // not primary type 
+            retType
+        }
+      }
+    }
+    state
+  }
+
+}
