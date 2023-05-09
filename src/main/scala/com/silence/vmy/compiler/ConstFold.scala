534c48a2b3f953d1be13baaf24ccc5d2190cfc44
forsilence <3312034934@qq.com>
2023-05-09 22:48:13 +0800

    TreeEmulator need Compiler as parameter

--- src/main/scala/com/silence/vmy/compiler/ConstFold.scala
+++ src/main/scala/com/silence/vmy/compiler/ConstFold.scala
@@ -4,10 +4,11 @@ import com.silence.vmy.compiler.tree._
 import com.silence.vmy.compiler.tree.Tree.Tag
 import com.silence.vmy.shared._
 import com.silence.vmy.evaluate._
+import com.silence.vmy.LCompiler
 
 trait ConstFold extends PerCompileUnitTVisitor {
   import EmulatingValue._
-  private val constExpressionEvaluator = new TreeEmulator(new EmulatorContext())
+  private val constExpressionEvaluator = new TreeEmulator(new EmulatorContext(), LCompiler)
   override def leaveUnary(expression: Unary, t : ContextType): Tree = {
     unfoldUnary(expression, t)
   }
