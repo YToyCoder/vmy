3ba3b14bac025f8494b8484fe3e291a28bf31d38
forsilence <3312034934@qq.com>
2023-05-09 21:59:14 +0800

    UpValue and Variable Declaration

---
+++ src/main/scala/com/silence/vmy/compiler/UpValues.scala
@@ -0,0 +1,8 @@
+package com.silence.vmy.compiler
+
+import com.silence.vmy.evaluate.EmulatorContext
+import com.silence.vmy.compiler.tree._
+
+class VariableDeclarationAndUpValuesChecking extends TVisitor[EmulatorContext] 
+{
+}
