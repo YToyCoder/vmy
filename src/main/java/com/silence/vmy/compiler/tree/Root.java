8969e48ae73994c3dab923c364811792650e0cf8
forsilence <3312034934@qq.com>
2023-05-14 18:51:55 +0800

    add root api : imports and exports

--- src/main/java/com/silence/vmy/compiler/tree/Root.java
+++ src/main/java/com/silence/vmy/compiler/tree/Root.java
@@ -1,5 +1,9 @@
 package com.silence.vmy.compiler.tree;
 
+import java.util.List;
+
 public interface Root extends Tree{
   Tree body();
+  default List<ImportState> imports(){ return List.of(); }
+  default List<ExportState> exports(){ return List.of(); }
 }
