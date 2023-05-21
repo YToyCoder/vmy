4ebc679a1ce3f13520e63a8f4b99a4abcd80edab
forsilence <3312034934@qq.com>
2023-05-21 21:14:48 +0800

    tree printer

--- src/main/scala/com/silence/vmy/evaluate/Loader.scala
+++ src/main/scala/com/silence/vmy/evaluate/Loader.scala
@@ -10,13 +10,16 @@ import java.io.FileNotFoundException
 import com.silence.vmy.tools.Utils.error
 import com.silence.vmy.compiler.CompileUnit.wrapAsCompileUnit
 import com.silence.vmy.compiler.LCompiler
+import com.silence.vmy.tools.TreePrinter
 
 class Loader(emulator: TreeEmulator) extends Log {
+  private val printer = new TreePrinter()
   def load(uri: String): Option[VModule] = {
     doParsing(uri) match
       case None => None
       case Some(value) => 
         val compiledUnit = LCompiler.compile(emulator.context, value)
+        // println(printer.tree_as_string(compiledUnit.node()))
         emulator.run(compiledUnit.node()) match
           case null => None
           case _ => None
